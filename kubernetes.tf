b91275b177e929ca610f6006392edbfad373860c
nickillgen <nickillgenpersonal@gmail.com>
2022-07-21 15:20:05 -0600

    remove ingress, listen on 00000

--- kubernetes.tf
+++ kubernetes.tf
@@ -81,29 +81,6 @@ resource "kubernetes_service" "ibm-rest-api" {
       target_port = 5000
       protocol    = "TCP"
     }
-    type = "NodePort"
-  }
-}
-
-resource "kubernetes_ingress" "ibm-ingress" {
-  wait_for_load_balancer = true
-  metadata {
-    name = "ibm-ingress"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
-  spec {
-    rule {
-      http {
-        path {
-          path = "/*"
-          backend {
-            service_name = kubernetes_service.ibm-rest-api.metadata.0.name
-            service_port = 5000
-          }
-        }
-      }
-    }
+    type = "LoadBalancer"
   }
 }
