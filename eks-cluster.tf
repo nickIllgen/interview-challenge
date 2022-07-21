65360714fe4e0e6ea0bdb3c656755a63beb022d4
nickillgen <nickillgenpersonal@gmail.com>
2022-07-21 13:08:25 -0600

    Try this

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -35,3 +35,32 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
+
+
+data "aws_iam_policy_document" "kubectl_assume_role_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::809031430406:root"]
+    }
+  }
+}
+resource "aws_iam_role" "eks_kubectl_role" {
+  name               = "example-kubectl-access-role"
+  assume_role_policy = "${data.aws_iam_policy_document.kubectl_assume_role_policy.json}"
+}
+resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
+}
+resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
+}
+resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
+}
