e79305459edb191a15997766acbf505985b68e62
nickillgen <nickillgenpersonal@gmail.com>
2022-07-20 19:11:27 -0600

    add vars

--- kubernetes.tf
+++ kubernetes.tf
@@ -56,11 +56,11 @@ resource "kubernetes_deployment" "ibm-rest-api" {
           name  = var.application_name
           env {
             name  = "AWS_ACCESS_KEY_ID"
-            value = var.aws-access-key-id
+            value = var.AWS_ACCESS_KEY_ID
           }
           env {
             name  = "AWS_SECRET_ACCESS_KEY"
-            value = var.aws-access-key-id
+            value = var.AWS_SECRET_ACCESS_KEY
           }
           env {
             name  = "REGION_NAME"
--- variables.tf
+++ variables.tf
@@ -6,5 +6,11 @@ variable "application_name" {
   type    = string
   default = "ibm-rest-api"
 }
-variable "aws-access-key-id" {}
-variable "aws-secret-access-key" {}
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  default = ""
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  default = ""
+}
