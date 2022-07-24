8578e0e55eb8d04eaa6c7d52e511b0218f38719f
nickillgen <nickillgenpersonal@gmail.com>
2022-07-24 12:04:56 -0600

    Final commit

--- variables.tf
+++ variables.tf
@@ -6,13 +6,3 @@ variable "application_name" {
   type    = string
   default = "ibm-rest-api"
 }
-
-variable "aws-access-key-id" {
-  type    = string
-  default = ""
-}
-
-variable "aws-secret-access-key" {
-  type    = string
-  default = ""
-}
